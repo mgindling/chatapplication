b520ba720f123e9dfbf7f482aab7f328590bad5c
mgindling <mgindlin@purdue.edu>
2017-11-14 14:22:47 -0500

    Began implementation of Broadcast and writeMessage

--- ChatServer.java
+++ ChatServer.java
@@ -3,8 +3,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 final class ChatServer {
     private static int uniqueId = 0;
@@ -17,7 +19,7 @@ final class ChatServer {
     }
 
     //Written constructor with no parameters.
-    private  ChatServer() {
+    private ChatServer() {
         this.port = 1500;
     }
 
@@ -71,6 +73,31 @@ final class ChatServer {
             }
         }
 
+        //Write message method. Returns false if there's no socket connection; returns true otherwise.
+        public boolean writeMessage(String message) {
+            //Tests if the socket is still open (I think).
+            if (socket.isConnected()) {
+                try {
+                    Date time = new Date(); //Includes date. Don't think this is how it's supposed to be done.
+                    sOutput.writeObject(message + " " + time); //Writes message to the output stream which lets other clients see it.
+                    return true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+
+        //Broadcast method. Sends message to all clients. Needs to be concurrent.
+        public void Broadcast(String message) {
+            for (int c = 0; c < clients.size(); c++) {
+                clients.get(c).writeMessage(message);
+            }
+        }
 
 
         //This is what the client thread actually runs.
@@ -94,3 +121,4 @@ final class ChatServer {
         }
     }
 }
+
